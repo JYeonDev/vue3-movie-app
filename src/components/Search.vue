000bfff5584209ade161b368046dd5a031837fcf
JYeonDev <whddus4837@gmail.com>
2021-08-14 19:57:29 +0900

    Search - 필터

---
+++ src/components/Search.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <input
+      v-model="title"
+      class="form-control"
+      type="text" 
+      placeholder="Search for Movies, Series & more" />
+    <div class="selects">
+      <select
+        v-for="filter in filters"
+        v-model="$data[filter.name]"
+        :key="filter.name"
+        class="form-select">
+        <option
+          v-if="filter.name === 'year'"
+          value="">
+          All Years
+        </option>
+        <option
+          v-for="item in filter.items"
+          :key="item">
+          {{ item }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      type: 'movie',
+      number: 10,
+      year: '',     /* template 부분에 option 부분에 value가 비어있는데 비어있는 경우 All Years 가 출력된다. */
+      filters: [
+        {
+          name: 'type',
+          items: ['movie', 'series', 'episode']
+        },
+        {
+          name: 'number',
+          items: [10, 20, 30]
+        },
+        {
+          name: 'year',
+          items: (() => {
+            const years = []
+            const thisYear = new Date().getFullYear()  // 자동으로 현재 년도 갱신
+            for (let i = thisYear; i >= 1985; i-= 1) {
+              years.push(i)
+            }
+            return years
+          })()
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    > * {
+      margin-right: 10px;
+      font-size: 15px;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+    display: flex;
+    .selects {
+      display: flex;
+      select {
+        width: 120px;
+        margin-right: 10px;
+        &:last-child {
+          margin-right: 0;
+        }
+      }
+    }
+  }
+</style>
