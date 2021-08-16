3d0463ca39a0bebd150f83394bc769a189d9aacd
JYeonDev <whddus4837@gmail.com>
2021-08-16 16:11:54 +0900

    영화 검색

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -3,20 +3,21 @@
     <div class="inner">
       <MovieItem
         v-for="movie in movies"
-        :key="movie.imdbID" />
+        :key="movie.imdbID" 
+        :movie="movie" />
     </div>
   </div>
 </template>
 
 <script>
-import MovieItem from '~components/MovieItem'
+import MovieItem from '~/components/MovieItem'
 export default {
   components: {
     MovieItem
   },
-  data() {
-    return {
-      movies: []
+  computed: {
+    movies() {
+      return this.$store.state.movie.movies
     }
   }
 }
--- src/store/movie.js
+++ src/store/movie.js
@@ -1,27 +1,40 @@
+import axios from 'axios'
+
 export default {
   // module!  module화되서 사용
   namespaced: true,
   // data!    취급해야하는 데이터를 의미
   state: () => ({
-    movies: []
+    movies: [],
+    message: '',
+    loading: false
   }),
   // computed!    계산된 데이터를 만든다.
-  getters: {
-    movieIds(state) {
-      return state.movies.map(m => m.imdbId)
-    }
-  },
+  getters: {},
   // methods!     함수를 만들어 활용
   // 변이     데이터를 변경 시킬수 있다. mutations를 통해서만 변경이 가능하다.
   mutations: {
+    updateState(state, payload) {
+      // ['movies', 'message', 'loading']
+      Object.keys(payload).forEach(key => {
+        state[key] = payload[key]
+      })
+    },
     resetMovies(state) {
       state.movies = []
     }
   },
   // 비동기
   actions: {
-    searchMovies() {
-      
+    async searchMovies({commit}, payload) {
+      const {title, type, number, year} = payload
+      // Search Movie..
+      const OMDB_API_KEY = '7035c60c'
+      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
+      const {Search, totalResult} = res.data
+      commit('updateState', {
+        movies: Search
+      })
     }
   }
 }
