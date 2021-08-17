8f256d301d01aae84598ce62bb5dd4cdf09fa3bc
JYeonDev <whddus4837@gmail.com>
2021-08-17 13:01:05 +0900

    영화 검색 추가 요청

--- src/store/movie.js
+++ src/store/movie.js
@@ -26,15 +26,32 @@ export default {
   },
   // 비동기
   actions: {
-    async searchMovies({commit}, payload) {
+    async searchMovies({state, commit}, payload) {
       const {title, type, number, year} = payload
       // Search Movie..
       const OMDB_API_KEY = '7035c60c'
       const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
-      const {Search, totalResult} = res.data
+      const {Search, totalResults} = res.data
       commit('updateState', {
         movies: Search
       })
+      console.log(totalResults) // 관련영화의 갯수
+      console.log(typeof totalResults)  // string
+
+      const total = parseInt(totalResults, 10)
+      const pageLength = Math.ceil(total / 10)
+
+      // 추가 요청!
+      if (pageLength > 1) {
+        for (let page = 2; page <= pageLength; page +=1) {
+          if (page > (number / 10)) break
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+          const {Search} = res.data
+          commit('updateState', {
+            movies: [...state.movies, ...Search]
+          })
+        }
+      }
     }
   }
 }
